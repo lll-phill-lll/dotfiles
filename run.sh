f6267c0a3da62042a1bdec624b4e1c29efff2afe
Filitov Mikhail <filitovme@mail.ru>
2021-02-01 12:12:01 +0300

    fixed links

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # create symlink to vimrc
-ln -s .vimrc ~/.vimrc
+ln -n .vimrc ~/.vimrc
 #install plugins
 vim +PlugInstall +qall > /dev/null
 # install russian mac keymap
@@ -30,8 +30,8 @@ wget http://ftp.vim.org/vim/runtime/spell/en.ascii.spl -P ~/.vim/spell
 # install custom colorsheme from .gl files
 mkdir -p ~/.vim/syntax
 mkdir -p ~/.vim/ftdetect
-ln -s ft_goals.vim ~/.vim/ftdetect/goals.vim
-ln -s syn_goals.vim ~/.vim/syntax/goals.vim
+ln -n ft_goals.vim ~/.vim/ftdetect/goals.vim
+ln -n syn_goals.vim ~/.vim/syntax/goals.vim
 
 #install tmux
 if [ ${machine} == "Mac" ]; then
@@ -40,10 +40,10 @@ elif [ ${machine} == "Linux" ]; then
     sudo apt install tmux
 fi
 # create a symlink to tmux.conf
-ln -s .tmux.conf ~/.tmux.conf
+ln -n .tmux.conf ~/.tmux.conf
 
 # install itunes artist for mac (for tmux airline)
 if [ ${machine} == "Mac" ]; then
-    ln -s itunesartist ~/itunesartist
-    ln -s itunestrack ~/itunestrack
+    ln -n itunesartist ~/itunesartist
+    ln -n itunestrack ~/itunestrack
 fi
